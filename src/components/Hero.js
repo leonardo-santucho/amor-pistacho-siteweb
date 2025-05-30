c3562841422027b9fddf4ec5b0d42e6e10b9da2b
Leonardo Santucho <leosantucho@gmail.com>
2025-05-29 23:47:15 -0300

    update text on home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,23 @@
 import { motion } from "framer-motion";
 
 export default function Hero() {
-  return (
+return (
     <div className="relative h-screen w-full">
-      <img
-        src="/portada-01.jpg"
-        alt="Amor Pistacho"
-        className="absolute inset-0 w-full h-full object-cover"
-      />
-      <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
-      <motion.h1
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-            className="text-white text-5xl md:text-7xl font-bold italic font-raleway tracking-wide drop-shadow-lg"
+        <img
+            src="/portada-01.jpg"
+            alt="Amor Pistacho"
+            className="absolute inset-0 w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
+            <motion.h1
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                className="text-white text-5xl md:text-7xl font-bold italic font-raleway tracking-wide drop-shadow-lg text-center"
             >
-            amor pistacho
-        </motion.h1>
-      </div>
+                amor<br />pistacho
+            </motion.h1>
+        </div>
     </div>
-  );
+);
 }
